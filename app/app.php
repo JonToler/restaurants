9952d2a34ca44bb9e8dafc7afb49935be1c515cf
Jon Toler and Seth Kendall <jon@>
2016-09-22 16:05:37 -0700

    add database search functionality

--- app/app.php
+++ app/app.php
@@ -25,37 +25,55 @@
     $app->get("/", function() use ($app) {
     $restaurants_found = array();
     $temp_var = array();
-    return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants_found, 'temp' => $temp_var));
+    return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants_found, 'not_found' => $temp_var));
     });
 
     $app->post("/cuisine_search", function() use ($app) {
-    $search_term = $_POST['cuisine'];
-    // $cuisine_result = $GLOBALS['DB']->query("SELECT * FROM cuisine WHERE name = '{$search_term}'");
-
     $all_cuisines = Cuisine::getAll();
-    $found_it = null;
-    $temp_var = "found";
     $restaurants_found = array();
-    foreach ($all_cuisines as $cuisine) {
-        $cuisine_name = $cuisine->getName();
+    $cuisines_found = array();
+    $cuisines_not_found = array();
+    $not_found_message = array();
+    $search_term_holder1 = $_POST['cuisine'];
+    $search_term_holder2 = explode(",", $search_term_holder1);
+    foreach ($search_term_holder2 as $term) {
+      $search_term1 = strtolower($term);
+      $search_term = str_replace(' ', '', $search_term1);
+      $numberOfTerms = count($all_cuisines);
+      for ($i=0; $i < $numberOfTerms ; $i++) {
+        $cuisine_name = $all_cuisines[$i]->getName();
         if ($cuisine_name == $search_term) {
-            $found_it = $cuisine;
+          array_push($cuisines_found, $all_cuisines[$i]);
+          break;
+        }elseif ($i == ($numberOfTerms-1)) {
+          array_push($cuisines_not_found, $search_term);
         }
+      }
     }
-    if ($found_it == null) {
-        $temp_var = "not found";
+    foreach ($cuisines_found as $cuisine) {
+      $restaurants_matching_search_term = $cuisine->getRestaurants();
+      foreach ($restaurants_matching_search_term as $restaurant) {
+        array_push($restaurants_found, $restaurant);
+      }
     }
-    // $new_name = $cuisine_result['name'];
-    // $new_id = $cuisine_result['id'];
-    // $returned_cuisine = new Cuisine($new_name, $new_id);
-    // $returned_cuisine2 = Cuisine::find($returned_cuisine->getId());
-    if ($found_it != null) {
-        $restaurants_found = $found_it->getRestaurants();
+    $not_found_check = empty($cuisines_not_found);
+    if ($not_found_check == false) {
+        $not_found_message = "No results found for:";
+        return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants_found, 'not_found' => $not_found_message, 'not_found_terms' => $cuisines_not_found));
     }
 
+    return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants_found, 'not_found' => $not_found_message));
+    });
+    $app->get("/add_cuisine", function() use ($app) {
 
-
-    return $app['twig']->render('index.html.twig', array('restaurants' => $restaurants_found, 'temp' => $temp_var));
+        return $app['twig']->render('new_cuisine.html.twig');
+    });
+    $app->post("/save_new_cuisine", function() use ($app) {
+        $cuisine = $_POST['cuisine'];
+        $id = null;
+        $new_cuisine = new Cuisine($cuisine, $id);
+        $new_cuisine->save();
+        return $app->redirect('/');
     });
     return $app;
 ?>
